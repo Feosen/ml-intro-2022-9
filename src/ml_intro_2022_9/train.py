5ad5a0f546f1b61ce9c8ce51ca2be2dee748141d
Feosen <iva588@yandex.ru>
2022-04-24 20:27:37 +0300

    feat: add train script placeholder

---
+++ src/ml_intro_2022_9/train.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import click
+
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/heart.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
+def train(
+    dataset_path: Path,
+    save_model_path: Path,
+    random_state: int,
+) -> None:
+    print(dataset_path, save_model_path, random_state)
