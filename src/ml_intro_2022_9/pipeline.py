4551ff3112f5d0bd1fe87e1859066188bf00e38f
Feosen <iva588@yandex.ru>
2022-04-29 01:09:14 +0300

    feat: add random-forest.

--- src/ml_intro_2022_9/pipeline.py
+++ src/ml_intro_2022_9/pipeline.py
@@ -1,14 +1,27 @@
+from sklearn.ensemble import RandomForestClassifier  # type: ignore
 from sklearn.neighbors import KNeighborsClassifier  # type: ignore
 from sklearn.pipeline import Pipeline  # type: ignore
 from sklearn.preprocessing import StandardScaler  # type: ignore
 
 
-def create_pipeline(n_neighbors: int) -> Pipeline:
+def create_knn_pipeline(n_neighbors: int) -> Pipeline:
+    """Build KNN pipline."""
     pipeline_steps = (
         ("scaler", StandardScaler()),
         (
             "classifier",
             KNeighborsClassifier(n_neighbors=n_neighbors, n_jobs=-1),
+        ),
+    )
+    return Pipeline(steps=pipeline_steps)
+
+
+def create_rf_pipeline() -> Pipeline:
+    """Build Random Forest pipline"""
+    pipeline_steps = (
+        (
+            "classifier",
+            RandomForestClassifier(n_jobs=-1),
         ),
     )
     return Pipeline(steps=pipeline_steps)
--- src/ml_intro_2022_9/train.py
+++ src/ml_intro_2022_9/train.py
@@ -1,14 +1,25 @@
+from enum import Enum
 from pathlib import Path
+from typing import List
 
 import click
 import mlflow  # type: ignore
-import numpy as np
+import numpy as np  # type: ignore
 from joblib import dump  # type: ignore
 from sklearn.model_selection import cross_validate  # type: ignore
 
 from .data import get_dataset
 from .git import get_git_revision_hash
-from .pipeline import create_pipeline
+from .pipeline import create_rf_pipeline, create_knn_pipeline
+
+
+class ModelName(Enum):
+    knn = "knn"
+    random_forest = "random-forest"
+
+    @classmethod
+    def values(cls) -> List[str]:
+        return [v.value for v in cls]
 
 
 @click.command()
@@ -24,6 +35,13 @@ from .pipeline import create_pipeline
     "--save-model-path",
     default="data/model.joblib",
     type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-m",
+    "--model-name",
+    default="knn",
+    type=click.Choice(ModelName.values()),
     show_default=True,
 )
 @click.option(
@@ -47,6 +65,7 @@ from .pipeline import create_pipeline
 def train(
     dataset_path: Path,
     save_model_path: Path,
+    model_name: str,
     random_state: int,
     cv: int,
     n_neighbors: int,
@@ -54,9 +73,12 @@ def train(
     features, target = get_dataset(dataset_path, random_state)
     with mlflow.start_run():
         mlflow.log_param("git_rev_hash", get_git_revision_hash())
-
-        pipeline = create_pipeline(n_neighbors=n_neighbors)
-        mlflow.log_param("n_neighbors", n_neighbors)
+        mlflow.log_param("model_name", model_name)
+        if model_name == ModelName.knn.value:
+            pipeline = create_knn_pipeline(n_neighbors=n_neighbors)
+            mlflow.log_param("n_neighbors", n_neighbors)
+        elif model_name == ModelName.random_forest.value:
+            pipeline = create_rf_pipeline()
 
         scoring = ["accuracy", "f1_micro", "f1_macro"]
         results = cross_validate(
